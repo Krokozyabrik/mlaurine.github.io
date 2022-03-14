530a294747607a1e234bf7638401c89b2dd15b5a
Vitaliy <57210139+mlaurine@users.noreply.github.com>
2022-03-14 18:25:04 +0300

    update js

--- src/js/calc.js
+++ src/js/calc.js
@@ -2,36 +2,28 @@
 let sumTime = document.querySelector(".term");
 sumTime.innerText = 0;
 
-
 //стоимость
 let sumCost = document.querySelector(".rezCost");
 sumCost.innerText = 0;
 
-
 let sum = 0;
 let days = 5;
-
 
 $(".type").on("change", function() {
     if (parseInt($(this).val()) > 0) {
         sum = sum + parseInt($(this).val()) + (days * 2000);
         days = days + (parseInt($(this).val()) / 200);
-
     } else {
         (parseInt($(this).val()) == 0)
         sum = 0;
         days = 0;
     }
 
-
-
     let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum.toFixed(0);
-
     let sumTime = document.querySelector(".term");
     sumTime.innerText = days.toFixed(0);
 });
-
 
 $(".dezine").on("change", function() {
     if (parseInt($(this).val()) > 0) {
@@ -43,7 +35,6 @@ $(".dezine").on("change", function() {
         days = 0;
     }
 
-
     let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum.toFixed(0);
     let sumTime = document.querySelector(".term");
@@ -51,7 +42,6 @@ $(".dezine").on("change", function() {
 });
 
 $(".adaptive").on("change", function() {
-
     if (parseInt($(this).val()) > 0) {
         sum = sum + parseInt($(this).val()) + (days * 500);
         days = days + (parseInt($(this).val()) / 200)
@@ -60,7 +50,6 @@ $(".adaptive").on("change", function() {
         sum = 0;
         days = 0;
     }
-
 
     let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum.toFixed(0);
