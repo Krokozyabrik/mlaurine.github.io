0d5475312fa2c1d31a8f5b75848eaa5bb8e91659
Vitaliy <57210139+mlaurine@users.noreply.github.com>
2022-03-14 14:15:45 +0300

    Add files via upload

--- src/js/calc.js
+++ src/js/calc.js
@@ -8,47 +8,40 @@ let sumCost = document.querySelector(".rezCost");
 sumCost.innerText = 0;
 
 
+let sum = 0;
+let days = 5;
+
 
 $(".type").on("change", function() {
-    let sum = 0;
-    let days = 5;
     sum = sum + parseInt($(this).val()) + (days * 2000);
     days = days + parseInt($(this).val())
+
     let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum;
-
     let sumTime = document.querySelector(".term");
     sumTime.innerText = days;
 });
 
+
 $(".dezine").on("change", function() {
-    let sum = 0;
-    let days = 5;
     sum = sum + parseInt($(this).val()) + (days * 2000);
     days = days + parseInt($(this).val())
+
+
     let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum;
-
     let sumTime = document.querySelector(".term");
     sumTime.innerText = days;
 });
 
 $(".adaptive").on("change", function() {
-    let sum = 0;
-    let days = 5;
+
     sum = sum + parseInt($(this).val()) + (days * 2000);
     days = days + parseInt($(this).val())
+
+
     let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum;
-
     let sumTime = document.querySelector(".term");
     sumTime.innerText = days;
 });
-
-//сроки
-/*
-$(.type + " option:selected").attr("days");
-let days = 5;
-
-let sumTime = document.querySelector(".term");
-sumTime.innerText = 0;*/
