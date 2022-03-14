45bbf0f6a67bde58b33d71aa049a2f1c88499603
Vitaliy <57210139+mlaurine@users.noreply.github.com>
2022-03-14 11:01:41 +0300

    Add files via upload

--- src/js/calc.js
+++ src/js/calc.js
@@ -8,6 +8,7 @@ let sumCost = document.querySelector(".rezCost");
 sumCost.innerText = 0;
 
 
+
 $(".type").on("change", function() {
     let sum = 0;
     let days = 5;
@@ -20,6 +21,29 @@ $(".type").on("change", function() {
     sumTime.innerText = days;
 });
 
+$(".dezine").on("change", function() {
+    let sum = 0;
+    let days = 5;
+    sum = sum + parseInt($(this).val()) + (days * 2000);
+    days = days + parseInt($(this).val())
+    let sumCost = document.querySelector(".rezCost");
+    sumCost.innerText = sum;
+
+    let sumTime = document.querySelector(".term");
+    sumTime.innerText = days;
+});
+
+$(".adaptive").on("change", function() {
+    let sum = 0;
+    let days = 5;
+    sum = sum + parseInt($(this).val()) + (days * 2000);
+    days = days + parseInt($(this).val())
+    let sumCost = document.querySelector(".rezCost");
+    sumCost.innerText = sum;
+
+    let sumTime = document.querySelector(".term");
+    sumTime.innerText = days;
+});
 
 //сроки
 /*
