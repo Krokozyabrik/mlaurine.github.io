6e267f074af001297c7c589dfcca7d227885fbeb
Vitaliy <57210139+mlaurine@users.noreply.github.com>
2022-03-15 21:57:00 +0300

    Add files via upload

--- src/js/calc.js
+++ src/js/calc.js
@@ -7,52 +7,51 @@ let sumCost = document.querySelector(".rezCost");
 sumCost.innerText = 0;
 
 let sum = 0;
-let days = 5;
+let days = 0;
+let sum1 = 0;
+let days1 = 0;
+let sum2 = 0;
+let days2 = 0;
 
 $(".type").on("change", function() {
     if (parseInt($(this).val()) > 0) {
-        sum = sum + parseInt($(this).val()) + (days * 2000);
-        days = days + (parseInt($(this).val()) / 200);
+        sum = parseInt($(this).val()) + (5 * 2000);
+        days = 5 + (parseInt($(this).val()) / 200);
     } else {
         (parseInt($(this).val()) == 0)
         sum = 0;
         days = 0;
     }
 
-    let sumCost = document.querySelector(".rezCost");
     sumCost.innerText = sum.toFixed(0);
-    let sumTime = document.querySelector(".term");
     sumTime.innerText = days.toFixed(0);
 });
 
 $(".dezine").on("change", function() {
     if (parseInt($(this).val()) > 0) {
-        sum = sum + parseInt($(this).val()) + (days * 1000);
-        days = days + (parseInt($(this).val()) / 200)
+        sum1 = parseInt($(this).val()) + (5 * 1000);
+        days1 = 5 + (parseInt($(this).val()) / 200)
     } else {
         (parseInt($(this).val()) == 0)
-        sum = 0;
-        days = 0;
+        sum1 = 0;
+        days1 = 0;
     }
 
-    let sumCost = document.querySelector(".rezCost");
-    sumCost.innerText = sum.toFixed(0);
-    let sumTime = document.querySelector(".term");
-    sumTime.innerText = days.toFixed(0);
+    sumCost.innerText = sum.toFixed(0) + sum1.toFixed(0);
+    sumTime.innerText = days.toFixed(0) + days1.toFixed(0);
 });
+
 
 $(".adaptive").on("change", function() {
     if (parseInt($(this).val()) > 0) {
-        sum = sum + parseInt($(this).val()) + (days * 500);
-        days = days + (parseInt($(this).val()) / 200)
+        sum2 = parseInt($(this).val()) + (5 * 500);
+        days2 = 5 + (parseInt($(this).val()) / 200)
     } else {
         (parseInt($(this).val()) == 0)
-        sum = 0;
-        days = 0;
+        sum2 = 0;
+        days2 = 0;
     }
 
-    let sumCost = document.querySelector(".rezCost");
-    sumCost.innerText = sum.toFixed(0);
-    let sumTime = document.querySelector(".term");
-    sumTime.innerText = days.toFixed(0);
+    sumCost.innerText = sum.toFixed(0) + sum1.toFixed(0) + sum2.toFixed(0);
+    sumTime.innerText = days.toFixed(0) + days1.toFixed(0) + days2.toFixed(0);
 });
